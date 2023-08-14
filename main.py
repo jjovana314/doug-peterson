197b64eeb90ca368ea172b9f6c1e2cee304a76ee
Jovana Jovanovic <jovana@ggstud.io>
2023-08-14 18:02:46 +0200

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+from discordLevelingSystem import DiscordLevelingSystem, LevelUpAnnouncement, RoleAward
+from mee6_py_api import API
+import os
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv()
+
+
