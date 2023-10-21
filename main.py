d3de735c87172d50eb7b72f5a761cfe2586bf459
Jovana Jovanovic <jjovana314@gmail.com>
2023-10-21 19:22:05 +0200

    updated module; env file added

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 import discord
 from discord.ext import commands
 from datetime import datetime, timedelta
+from decouple import config
 
 # configuration
-TOKEN = 'YOUR_BOT_TOKEN'
-PREFIX = '!'
-LURKER_ROLE_NAME = 'Lurker'
-INACTIVE_THRESHOLD = 7
+token = config('TOKEN')
+prefix = config('PREFIX')
+assign_role = config('ASSIGN_ROLE_NAME')
+trust_role_name = config('TRUST_ROLE_NAME')
+inactive_threshold = config('INACTIVE_THRESHOLD')
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.members = True
 
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command()
+async def get_roles(ctx) -> list:
+    # add members
+    server = ctx.guild
+    member = ctx.author  # command author
+
+    # add existing roles
+    roles = member.roles
+
+    # return roles
+    role_names = [role.name for role in roles]
+    await ctx.send(f'{member.name} has roles: {", ".join(role_names)}')
+    return role_names
+
 
 @bot.event
 async def on_message(message):
@@ -24,17 +44,30 @@ async def on_message(message):
     await bot.process_commands(message)
     user = message.author
     last_message_time = user.created_at
+
     for msg in await user.history(limit=None).flatten():
         if msg.created_at > last_message_time:
             last_message_time = msg.created_at
     time_difference = datetime.now() - last_message_time
 
+    lurker_role = discord.utils.get(message.guild.roles, name=assign_role)
+    trust_role = discord.utils.get(message.guild.roles, name=trust_role_name)
+
     # check user for lurker role
-    if time_difference.days >= INACTIVE_THRESHOLD:
-        lurker_role = discord.utils.get(message.guild.roles, name=LURKER_ROLE_NAME)
+    if trust_role in user.roles:
+        # user has trust role
+        return
+    elif time_difference.days >= inactive_threshold:
         await user.add_roles(lurker_role)
     else:
-        lurker_role = discord.utils.get(message.guild.roles, name=LURKER_ROLE_NAME)
         await user.remove_roles(lurker_role)
 
-bot.run(TOKEN)
+
+@bot.command()
+async def invite(ctx):
+    # generate link for server
+    link = await ctx.channel.create_invite()
+    await ctx.send(link)
+
+
+bot.run(token)
