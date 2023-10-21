e388dbc291dc02d727575a18bd3c6cbbac80824e
Jovana Jovanovic <jjovana314@gmail.com>
2023-10-21 18:31:43 +0200

    main module fixed

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
-import os
-from dotenv import load_dotenv
-import requests
 import discord
-from discord.ext import commands, tasks
-import asyncio
-from datetime import datetime, timedelta, timezone
+from discord.ext import commands
+from datetime import datetime, timedelta
 
-load_dotenv()
+# configuration
+TOKEN = 'YOUR_BOT_TOKEN'
+PREFIX = '!'
+LURKER_ROLE_NAME = 'Lurker'
+INACTIVE_THRESHOLD = 7
+
 intents = discord.Intents.default()
 intents.typing = False
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents.presences = False
 
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-@bot.command()
-async def last_message(ctx, user: discord.User):
-    async for guild in bot.fetch_guilds(limit=None):
-        for channel in guild.text_channels:
-            async for message in channel.history(limit=None):
-                if message.author == user:
-                    last_message_date = message.created_at
-                    await ctx.send(f"{user.display_name}'s last message was sent on {last_message_date} in {channel.mention}.")
-                    return
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
 
+@bot.event
+async def on_message(message):
+    # track time of last message
+    await bot.process_commands(message)
+    user = message.author
+    last_message_time = user.created_at
+    for msg in await user.history(limit=None).flatten():
+        if msg.created_at > last_message_time:
+            last_message_time = msg.created_at
+    time_difference = datetime.now() - last_message_time
 
-@tasks.loop(hours=168)  # Run once every 7 days
-async def scheduled_task(channel_id: int, user_id: str):
-    now = datetime.now(timezone.utc)
-    target_time = datetime.combine(now.date(), datetime.min.time()) + timedelta(hours=3)
-    if now > target_time:
-        target_time += timedelta(days=1)
-    delay = (target_time - now).total_seconds()
+    # check user for lurker role
+    if time_difference.days >= INACTIVE_THRESHOLD:
+        lurker_role = discord.utils.get(message.guild.roles, name=LURKER_ROLE_NAME)
+        await user.add_roles(lurker_role)
+    else:
+        lurker_role = discord.utils.get(message.guild.roles, name=LURKER_ROLE_NAME)
+        await user.remove_roles(lurker_role)
 
-    await asyncio.sleep(delay)
-    # Call your last_message function here
-    channel = bot.get_channel(channel_id)  # Replace with your channel ID
-    user = discord.utils.get(channel.guild.members, id=user_id)  # Replace with the user's ID
-    await last_message.invoke(await bot.get_context(user))
-
-
-@scheduled_task.before_loop
-async def before_scheduled_task():
-    await bot.wait_until_ready()
+bot.run(TOKEN)
