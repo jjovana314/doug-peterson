3538fb2c1c2e0dfc7d234c553dceccee4c70ac6d
Jovana Jovanovic <jjovana314@gmail.com>
2024-03-02 00:53:19 +0100

    fixed issues with role adding and tested

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+from discord import Member, Message, Intents, utils
 from discord.ext import commands
 from decouple import config
 from datetime import timedelta, datetime
@@ -8,15 +8,17 @@ import time
 token = config('TOKEN')
 channel_id = config('CHANNEL_ID')
 lurker_role_id = int(config('LURKER_ROLE_ID'))
+bots_role_id = int(config('BOTS_ROLE_ID'))
 inactive_threshold = 10
 
 run_at = datetime.now() + timedelta(days=30)
 delay = (run_at - datetime.now()).total_seconds()
 
-intents = discord.Intents.all()
+intents = Intents.all()
 intents.typing = False
 intents.presences = False
 intents.members = True
+intents.moderation = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -24,26 +26,29 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command()
 async def add_lurker(ctx) -> None:
     """ This is a test method for now, just to see how scheduling system in discord library works """
-    lurker_role = discord.utils.get(ctx.guild.roles, id=lurker_role_id)
-    non_lurkers = [member.name for member in ctx.guild.members if lurker_role not in member.roles]
-    inactive_users = [member for member in ctx.guild.members if lurker_role not in member.roles]
+    print("Started adding lurker role to users...")
+    lurker_role = utils.get(ctx.guild.roles, id=lurker_role_id)
+    bots_role = utils.get(ctx.guild.roles, id=bots_role_id)
+    non_lurkers = [member for member in ctx.guild.members if lurker_role not in member.roles]
+    bots = [member for member in ctx.guild.members if bots_role in member.roles]
+    inactive_users: [Member] = [member for member in ctx.guild.members if lurker_role not in member.roles]
 
     if len(inactive_users) > 0:
         for member in inactive_users:
-            last_message = await get_last_message(member)
-            if last_message is not None and (discord.utils.utcnow() - last_message.cread_at).days > 60:
-                await member.add_role(lurker_role)
+            if member in non_lurkers and member not in bots:
+                last_message = await get_last_message(member)
+                if last_message is not None and (utils.utcnow() - last_message.created_at).days > 60:
+                    await member.add_roles(lurker_role)
+                    print(f"Lurker added for user {member.name} - id: {member.id}")
+            else:
+                print(f"User {member.name} id: {member.id} already has lurker role or is bot, skipping...")
 
 
-async def get_last_message(member):
-    try:
-        for channel in member.guild.text_channels:
-            last_message = await channel.history(limit=1).flatten()
-            if last_message:
-                return last_message[0]
-        return None
-    except IndexError:
-        return None
+async def get_last_message(member: Member) -> Message or None:
+    for channel in member.guild.text_channels:
+        async for message in channel.history(limit=100):  # Adjust the limit as needed
+            if message.author.id == member.id:
+                return message
 
 
 @bot.event
@@ -62,7 +67,6 @@ async def on_ready():
 async def get_roles(ctx) -> list:
     """ Getting user's roles """
     # add members
-    server = ctx.guild
     member = ctx.author  # command author
 
     # add existing roles
@@ -82,7 +86,7 @@ async def on_message(message):
     # track time of last message
     await bot.process_commands(message)
     user = message.author
-    lurker_role = discord.utils.get(user, name='lurker')
+    lurker_role = utils.get(user, name='lurker')
     if lurker_role in user.roles:
         await user.remove_role(lurker_role)
 
