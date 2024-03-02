81933a2e6d624c9f74cdcff6be39e1157cb37b6d
Jovana Jovanovic <jjovana314@gmail.com>
2024-03-02 21:22:13 +0100

    log to file added

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(formatter)
+file_handler = logging.FileHandler('doug_peterson.log')
+file_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 
 @bot.command()
